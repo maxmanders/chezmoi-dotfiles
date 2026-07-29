3028a4696eec7c7c6f7a4a8716a9eca4b9f4e71f
Max Manders <max@maxmanders.co.uk>
2026-07-29 11:26:38 +0100

    feat(gitconfig): tweaks to gitconfig and difftoo/nvimdiff

--- home/dot_config/nvim/lua/autocmds.lua
+++ home/dot_config/nvim/lua/autocmds.lua
@@ -25,6 +25,8 @@ vim.api.nvim_create_autocmd("VimEnter", {
     if not real_file and not no_name then return end
     -- Skip when opened as $EDITOR by git (commit messages, rebase todo, etc.)
     if data.file:find("[/\\]%.git[/\\]", 1) then return end
+    -- Skip when opened by git difftool
+    if vim.env.GIT_DIFF_PATH_COUNTER then return end
     -- Skip when opened as $EDITOR by gh or other tools (PR/issue bodies in tmp)
     local tmpdir = (os.getenv("TMPDIR") or "/tmp"):gsub("/$", "")
     if data.file:find("^/tmp/", 1, true) or data.file:find("^" .. vim.pesc(tmpdir) .. "/", 1) then return end
