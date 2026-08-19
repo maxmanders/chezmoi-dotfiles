abd3009f4c8e43796213c4ee557e5e0c059ec50a
Max Manders <max@maxmanders.co.uk>
2026-08-19 10:48:33 +0100

    fix(nvim): fix yaml lint issue

--- home/dot_config/nvim/lua/configs/lint.lua
+++ home/dot_config/nvim/lua/configs/lint.lua
@@ -8,16 +8,25 @@ lint.linters_by_ft = {
 }
 
 lint.linters.yamllint = vim.tbl_extend("force", lint.linters.yamllint, {
-  args = function()
-    if vim.fn.expand "%:t" == "Chart.yaml" then
-      return { "-d", "{extends: default, rules: {line-length: disable, empty-lines: {max-end: false}}}", "--format", "parsable", "-" }
-    end
-    return { "--format", "parsable", "-" }
-  end,
+  args = { "--format", "parsable", "-" },
+})
+
+lint.linters.yamllint_chart = vim.tbl_extend("force", lint.linters.yamllint, {
+  args = {
+    "-d",
+    "{extends: default, rules: {line-length: disable, empty-lines: {max-end: false}}}",
+    "--format",
+    "parsable",
+    "-",
+  },
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "InsertLeave" }, {
   callback = function()
-    lint.try_lint()
+    if vim.bo.filetype == "yaml" and vim.fn.expand "%:t" == "Chart.yaml" then
+      lint.try_lint("yamllint_chart")
+    else
+      lint.try_lint()
+    end
   end,
 })
